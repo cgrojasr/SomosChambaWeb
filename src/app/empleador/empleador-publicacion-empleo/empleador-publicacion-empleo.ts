0d40bf4cf78b5cc1d4ead079fddb1102186b2e06
Genaro Rojas <pciscroj@upc.edu.pe>
2026-04-27 21:29:33 -0500

    Semana 3 -  Ejemplo de padres e hijos

--- src/app/empleador/empleador-publicacion-empleo/empleador-publicacion-empleo.ts
+++ src/app/empleador/empleador-publicacion-empleo/empleador-publicacion-empleo.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-empleador-publicacion-empleo',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './empleador-publicacion-empleo.html',
   styleUrl: './empleador-publicacion-empleo.css',
 })
-export class EmpleadorPublicacionEmpleo {}
+export class EmpleadorPublicacionEmpleo {
+  private readonly route = inject(ActivatedRoute);
+//   private readonly destroyRef = inject(DestroyRef);
+
+  readonly publicacionId = signal<number | null>(null);
+
+  ngOnInit(): void {
+    this.route.params
+    //   .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((params: Params) => {
+        const id = Number(params['id']);
+        this.publicacionId.set(Number.isNaN(id) ? null : id);
+      });
+  }
+}
